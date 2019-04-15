12e0bd3d293ee2a125b7f133707649dd5d32a7ab
Kyle Gray <Kyle.Gray@availity.com>
2019-04-15 09:56:03 -0400

    refactor: added exports for pagination prepare types

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@
 
 import { composeWithPagination } from './composeWithPagination';
 import { preparePaginationResolver } from './paginationResolver';
+import { preparePaginationInfoTC, preparePaginationTC } from './types/preparePaginationType';
 
 export default composeWithPagination;
 
-export { composeWithPagination, preparePaginationResolver };
+export { composeWithPagination, preparePaginationResolver, preparePaginationInfoTC, preparePaginationTC };
 
 export type { ComposeWithPaginationOpts } from './paginationResolver';
